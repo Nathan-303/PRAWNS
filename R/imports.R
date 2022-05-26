87d20914e3db1dac89f9044c277de3dfbdcdb812
Aeranthos <92371087+Nathan-303@users.noreply.github.com>
2022-05-26 16:00:39 +0100

    Imported a load of functions and updated the namespace

---
+++ R/imports.R
@@ -0,0 +1,12 @@
+#' @import sf
+#' @import terra
+#' @import tidyverse
+#' @import ggplot2
+#' @import raster
+#' @import rlang
+#' @import ggpubr
+#' @import raster
+#' @import stars
+#' @import viridis
+#' @import colorspace
+NULL
