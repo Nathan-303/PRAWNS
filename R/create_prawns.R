8b5502b1a6596b4cb3cc9b6f242b7d75a75bc18d
Aeranthos <92371087+Nathan-303@users.noreply.github.com>
2022-06-01 15:01:23 +0100

    Removed adaptive filepath for national statistics, incorporated column renaming

--- R/create_prawns.R
+++ R/create_prawns.R
@@ -21,6 +21,10 @@
 #' @param key_variable_aliases Any alternate ways of namingf the key variable
 #' that are used in the different inputted tables, not necessary if the data is
 #' already nicely formatted so defaults to FALSE
+#'
+#' @param pollutant the pollutant being investigated, this should be in quotes and correspond with its name in the data
+#'
+#' @param year the year the data is from, used to standardise column names, currently doesnt work for years before 2000
 
 #' @keywords data
 #' @export
@@ -67,33 +71,77 @@ create_prawns <- function(raster_path,
 
 # Read the additional data as a list of tibbles----------------------------------------------------------------
 
-  additional_data <- list()
-  for (count in c(1:length(data_path))){
-    #Read the file as a tibble and hold it in a transition state whilst it's checked for errors
-    ts <- read.csv(data_path[count],row.names=1) %>% tibble()
-    #Check for a known formatting issue in the column names and correct it
-    if (colnames(ts)[1]== "ï..LSOA11CD"||colnames(ts)[1]== "LSOA.code..2011."){
-      colnames(ts)[1] <- "LSOA11CD"
-    }
-    #Store the transition state in the list of data
-    additional_data[count] <- ts
-  }
+  rural_urban <- read.csv("Data/LSOA_statistics/LSOA_urban_rural.csv")
 
-  #Create a tibble to collect all the additional data in
-  additional_data_tibble <- additional_data[1]
+  city_data <- read.csv("Data/LSOA_statistics/city lookup 2011.csv") %>%
+    tibble()
+  #Renames a column to avoid a special character that makes things go wrong
+  colnames(city_data)[1] <- "LSOA11CD"
 
-  #If there's more than one table of additional data, combine them all
-  if(length((additional_data)>1)){
-    for (count in c(2:length(additional_data))){
-      inner_join(additional_data_tibble,additional_data[count],by="LSOA11CD")
-    }}
+  #Reads the demographic information about the LSOAs, binds them by LSOA code so the FID is incorporated
+  LSOA_demographics <- read.csv("Data/LSOA_statistics/2019_LSOA_Stats.csv") %>%
+    tibble() %>%
+    rename(LSOA11CD=LSOA.code..2011.) %>%
+    inner_join(city_data,by="LSOA11CD")
+
+  #Links the demographic data to the references to the shapefile
+  demo_linked_reference <- inner_join(city_data,LSOA_demographics,by=c("LSOA11CD"="LSOA11CD","FID"="FID",
+                                                                       "LSOA11NM"="LSOA11NM","TCITY15CD"="TCITY15CD",
+                                                                       "TCITY15NM"="TCITY15NM")) %>%
+    inner_join(rural_urban,by=c("LSOA11NM"="LSOA11NM"))
+
+  #Reads the county lookup data
+  county_lookup <- read.csv("Data/LSOA_statistics/county lookup 2019.csv",row.names = 1)
+
+  #Makes a chunk for the LSOAs that are in counties, then mutates in a column saying it's a couty not UA
+  county_chunk <- inner_join(county_lookup,demo_linked_reference,by=c("LAD19NM"="Local.Authority.District.name..2019.")) %>%
+    mutate(Area_Type="County") %>%
+    rename("Area"=CTY19NM)
+
+  unitary_list <- !(LSOA_demographics$Local.Authority.District.name..2019. %in% county_lookup$LAD19NM)
+
+  unitary_chunk <- LSOA_demographics[unitary_list,] %>%
+    mutate(Area_Type="Unitary Authority",Area=Local.Authority.District.name..2019.)# %>%
+  #inner_join(demo_linked_reference,by="LSOA11CD")
+
+  refined_chunk <- bind_rows(unitary_chunk,county_chunk) %>% tibble()
 
 # Combine the pollution means with the additional data --------------------
 
 
   prawns <- inner_join(output,refined_chunk,by="LSOA11CD")
-  #Link the averages for each polygon with the additional data
-  #Save the results if a filepath was specified
+
+
+  renamer <- function(data,last_two_digits_year,pollutant_data_name){
+
+# Rename the columns for readability --------------------------------------
+
+
+    NamedList <- c("Agricultural","Domestic combustion","Energy production",
+                   "Industrial combustion","Industrial production","Natural",
+                   "Offshore","Other transport and mobile machinery","Road transport","Solvents","Total"
+                   ,"Total_no_points","Waste treatment and disposal")
+
+    Nmdlst <- paste0("mean.",c("agric","domcom","energyprod","indcom","indproc","nature","offshore","othertrans","roadtrans","solvents","total","totarea","waste","pntsrc"),pollutant_data_name,last_two_digits_year)
+
+    tracer <- colnames(data) %in% Nmdlst
+
+    #Finds the first position where matches start
+    starter <- detect_index(tracer,is_true)-1
+
+    for(index in 1:length(tracer)){
+      if(tracer[index] == TRUE){
+        colnames(data)[index] <- NamedList[index-starter]
+      }}
+
+    data
+  }
+
+  Superstack <- renamer(
+    data=Superstack,
+    last_two_digits_year=year-2000,
+    pollutant_data_name = pollutant_data_name
+  ) %>% mutate("Point sources"=Total-Total_no_points)
   #Return the resulting object
 
 # Output the results ------------------------------------------------------
