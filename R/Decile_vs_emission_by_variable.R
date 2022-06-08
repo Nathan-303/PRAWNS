28ff81906a0e736ca052f64b86c0c9abb2f11331
Aeranthos <92371087+Nathan-303@users.noreply.github.com>
2022-06-08 16:26:53 +0100

    buggy update to decilevs emission

--- R/Decile_vs_emission_by_variable.R
+++ R/Decile_vs_emission_by_variable.R
@@ -1,38 +1,43 @@
-#This fuction creates a graph of emissions vs decile for a chosen variable, 
+#This fuction creates a graph of emissions vs decile for a chosen variable,
 #it works off a variable already specified in R, this was chosen over a file path as
-#whilst it needs more steps it allows greater flexibility for a modular approach in the analysis 
+#whilst it needs more steps it allows greater flexibility for a modular approach in the analysis
 
 Decile_vs_emission_by_variable <- function(active_stack,chosen_decile,chosen_variable,chosen_grouping,title,xaxis,yaxis,UK_Average=FALSE,Pollutant="Chaos",Custom_scale=NULL,archive=FALSE){
- 
+
   active_graph <- ggplot(data=active_stack)+
-    aes(x={{chosen_decile}}, 
+    aes(x={{chosen_decile}},
         y={{chosen_variable}},
         colour={{chosen_grouping}}
     )+
-    
+
     scale_color_viridis(option="turbo", discrete = TRUE)+
-    
-    #geom_pointrange(na.rm=TRUE,stat="summary",fun.ymin=min,fun.ymax=max,fun.y=mean,) +
-    
-    coord_cartesian(xlim = c(1, 10),ylim=Custom_scale)+
-    
+
+    #coord_cartesian(xlim = c(1, 10),ylim=Custom_scale)+
+
     scale_x_continuous(
       breaks=c(1:10),
       expand = expansion(mult=0,add=0),
       minor_breaks = FALSE
     )+
-    
+
     labs(x=xaxis,
          y=yaxis,
          title=title
     )+
-    
-    geom_line(stat="summary"
+
+    geom_line(stat="summary",linetype="Mean"
     )+
-    
-    
-    geom_smooth(method="lm",formula=y~x,se=FALSE,show.legend=FALSE)
-    
+
+
+    geom_smooth(method="lm",formula=y~x,se=FALSE,show.legend=FALSE,linetype="Mean")+
+    #Plot the line of best fit for the median
+    geom_quantile(quantiles=0.5,
+                  aes(linetype="Median"),
+                  size =1)+
+
+    #Plot a line through the medians for each decile
+    geom_line(stat="summary",fun=median,aes(linetype="Median"))
+
 if(UK_Average==TRUE){
     #New section to add the extra line
    active_graph <- active_graph + geom_smooth(data=read.csv("Outputs/Output_data/NOx/NOxsuperstack2019.csv")
@@ -43,11 +48,13 @@ if(UK_Average==TRUE){
       #formula={{chosen_variable}}~{{chosen_decile}},
       se=FALSE,
       show.legend = FALSE,
-      colour="UK average"))
+      linetype="UK average"))+
+
+     scale_linetype_manual("UK average"=1,"Mean"=2, "Median"=3)
 }
 
-  
-  
+
+
   if(archive==TRUE){
     ggsave(filename=paste0("Outputs/Plots/",Pollutant,"/",title,".svg"),
            plot=last_plot(),
@@ -60,5 +67,5 @@ if(UK_Average==TRUE){
 
   }
   active_graph
- 
+
 }
