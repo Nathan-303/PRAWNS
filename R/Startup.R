c106a4f63171a8fcb3f490a113aba0949d541cc1
Aeranthos <92371087+Nathan-303@users.noreply.github.com>
2022-06-09 11:00:49 +0100

    Updated documentation and added startup function

---
+++ R/Startup.R
@@ -0,0 +1,18 @@
+#' @export
+#Load in all ther packages as a jerry rigged fix to figuring out namespace
+Startup <- function(){
+library(raster)
+library(sf)
+library(tidyverse)
+library(terra)
+library(exactextractr)
+library(rlang)
+library(broom)
+library(ggpubr)
+library(colorspace)
+library(viridis)
+library(googledrive)
+library(stars)
+library(devtools)
+library(roxygen2)
+}
