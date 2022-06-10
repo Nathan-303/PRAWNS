cf938caeb2f343dc7713af912cf8850ecd4c4586
Aeranthos <92371087+Nathan-303@users.noreply.github.com>
2022-06-10 15:14:16 +0100

    Set the faceting to an ifelse condition so that it worked

--- R/faceted_plot.R
+++ R/faceted_plot.R
@@ -2,8 +2,9 @@
 #' e.g. if grouped by city there would be a tile for every city.
 #' @param prawn_path the data source to use, should be a quoted csv filepath
 #'
-#' @param group Currently only takes "County" or "City" due to non standard
-#' evaluation, changes what the graph is faceted by
+#' @param group The group to facet by, can be TCITY15NM to group by city or
+#' anything else to group by county/UA, this is due to jerry rigging a path
+#' around non standard evaluation
 #'
 #' @param pollutant the name of the pollutant, used to name the graphs
 #' @export
@@ -16,8 +17,6 @@ filtered_data <- read.csv(prawn_path)
 City_profile <- ggplot(data=filtered_data)+
   aes(x=IMD,
       y=Total)+
-
-  facet_wrap(~TCITY15NM,scales="free_y")+
 
   scale_x_continuous(
     breaks=c(1:10),
@@ -68,5 +67,12 @@ City_profile <- ggplot(data=filtered_data)+
   scale_colour_manual(name="Line type",
                       breaks = c('Mean','Median','UK Average'),
                       values=c('Mean'='blue','Median'='red'))#,'UK Average'='black'))
+#Add on the facets conditional to the group variable
+if (group=="TCITY15NM"){
+  City_profile <- City_profile+ facet_wrap(~TCITY15NM,scales="free_y")
+  }else{
+    City_profile <- City_profile+
+    facet_wrap(~Area,scales="free_y")}
+
 City_profile
 }
