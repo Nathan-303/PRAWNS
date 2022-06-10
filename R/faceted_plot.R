dcadfdf56b6a11fff21c8486d1540d07461ad33d
Aeranthos <92371087+Nathan-303@users.noreply.github.com>
2022-06-10 14:48:06 +0100

    Created faceted plot, functional but the UK avereage is broken

---
+++ R/faceted_plot.R
@@ -0,0 +1,72 @@
+#' Creates a graph wit a little window showing every item in the group separately
+#' e.g. if grouped by city there would be a tile for every city.
+#' @param prawn_path the data source to use, should be a quoted csv filepath
+#'
+#' @param group the varaibel to facet by, should be unquoted and an exact match
+#' for the column name in the data
+#'
+#' @param pollutant the name of the pollutant, used to name the graphs
+#' @export
+#' faceted_plot()
+
+faceted_plot <- function(prawn_path,group,pollutant){
+
+filtered_data <- read.csv(prawn_path)
+
+City_profile <- ggplot(data=filtered_data)+
+  aes(x=IMD,
+      y=Total)+
+
+  facet_wrap(~TCITY15NM,scales="free_y")+
+
+  scale_x_continuous(
+    breaks=c(1:10),
+    minor_breaks = FALSE,
+    expand = expansion(mult=0,add=0))+
+
+
+  labs(x="IMD decile",
+       y=paste0(pollutant," emissions"),
+       title=paste0(" ", pollutant, "emission"))+
+
+  #Plot the line of best fit for the mean
+  geom_smooth(method="lm",
+              formula=y~x,
+              se=FALSE,
+              show.legend=FALSE,
+              aes(color='Mean'))+
+
+  #Plot a line passing through the mean at each decile
+  geom_line(stat="summary",aes(color='Mean'))+
+
+  #Plot the line of best fit for the median
+  geom_quantile(quantiles=0.5,
+                aes(color='Median'),
+                size =1)+
+
+  #Plot a line through the medians for each decile
+  geom_line(stat="summary",fun=median,aes(color='Median'))+
+
+  #Plot a regression line for the whole UK for comparison
+  geom_smooth(data=read.csv(prawn_path)
+              , aes(
+                x=IMD,
+                y=Total,
+                color='UK Average'),
+              method="lm",
+              formula=y~x,
+              se=FALSE,
+              show.legend = FALSE)+
+
+  #An extra line showing the UK average at each decile for comparison purposes
+  geom_line(data=read.csv(prawn_path),stat="summary" , aes(
+    x=IMD,
+    y=Total,colour='UK Average'),
+    show.legend = FALSE
+  )+
+
+  scale_colour_manual(name="Line type",
+                      breaks = c('Mean','Median','UK Average'),
+                      values=c('Mean'='blue','Median'='red','UK Average'='black'))
+City_profile
+}
