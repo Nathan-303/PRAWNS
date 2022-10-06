9403231ccf0043d2156baf63161893c0b302a774
Aeranthos <92371087+Nathan-303@users.noreply.github.com>
2022-10-06 13:37:52 +0100

    Sorted the legend and axis titles

--- R/area_IMD_vs_pol.R
+++ R/area_IMD_vs_pol.R
@@ -16,27 +16,35 @@ active_stack <- read.csv(prawn_path) %>% group_by(Area)
     active_stack <- read.csv(prawn_path) %>% group_by(TCITY15NM)
 }
 
-area_rank <- active_stack %>% summarise(mean =mean(Total),
-                                          median=median(Total),
+area_rank <- active_stack %>% summarise(Mean =mean(Total),
+                                          Median=median(Total),
                                           dep=mean(IMD)) %>%
-  pivot_longer(c(mean, median), names_to = "stat",values_to = "token") %>% group_by(stat)
+  pivot_longer(c(Mean, Median), names_to = "Average used for NOx",values_to = "token") %>% group_by(`Average used for NOx`)
 
 
 test <- ggplot(data=area_rank)+
 
   aes(x=dep,
       y=token,
-      shape=stat)+
+      shape=`Average used for NOx`,
+      linetype=`Average used for NOx`)+
 
   geom_point()+
 
   geom_smooth(method="lm",
               formula=y~x,
-              se=FALSE,
-              show.legend=FALSE)+
+              se=FALSE,)+
+
+  scale_linetype_manual(name="Linear regression",values=c(1,2))+
+
+  scale_shape_manual(name="Average used for NOx",
+                     values =c(16,4))+
+
+
+  theme(legend.key.size = unit(2,"lines"))+
 
   labs(x="Mean deprivation decile",
-       y=bquote("Average "~.(pollutant)~"emissions in "~.(year)~" /tonnes "~km^2),
+       y=bquote("Average "~.(pollutant)~"emissions in "~.(year)~"/ tonnes "~km^2),
        title=paste0("Deprivation vs ",pollutant, "emission, grouped by ",area_type))
 
 
