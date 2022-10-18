7174de787edddd352ae935be43d5bf84afa71290
Aeranthos <92371087+Nathan-303@users.noreply.github.com>
2022-10-18 16:18:17 +0100

    got it working

--- R/plot_county_gradients.R
+++ R/plot_county_gradients.R
@@ -7,8 +7,20 @@ refined <- raw %>% dplyr::select(LSOA11CD,IMD,Total,Area) %>% group_by(Area)
 
 coeffs <- refined %>%
   #get the rsquared
-  do(tidy(lm(Total~IMD, data=.)))
+  do(tidy(lm(Total~IMD, data=.))) %>% 
+  
+  filter(term=="IMD")
 
-graphable <- inner_join(models,coeffs %>% filter(term=="IMD"),by="Area")
+curiosity <- coeffs %>% filter(p.value<=0.05)
 
-curiosity <- graphable %>% filter(p.value)
+test <- ggplot()+
+  geom_histogram(data=coeffs,
+                 aes(x=estimate),bins=50)+
+  
+  geom_histogram(data=curiosity,
+                 aes(x=estimate),
+                 fill="orange",bins=50)+
+  
+  coord_cartesian(ylim=c(0,14),expand=FALSE)
+
+test
