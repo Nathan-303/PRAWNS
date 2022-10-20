a36c346c2bf2ebce47e01bdcf4149efada85f3c5
Aeranthos <92371087+Nathan-303@users.noreply.github.com>
2022-10-20 14:03:29 +0100

    Created average nox histogram

---
+++ R/avg_nox_histogram.R
@@ -0,0 +1,28 @@
+#' Create a histogram showign the gradients for every county/ua or city
+
+#' @param prawn_path The filepath for the prawn CSV that is to be used.
+#'
+
+#'
+#' @keywords faceted, sources
+#' @export
+#' @examples
+#' avg_nox_histogram()
+avg_nox_histogram <- function(prawn_path){
+  
+  raw <- read.csv(prawn_path,
+                  row.names=1,
+                  check.names=FALSE)
+ 
+  histo <- ggplot(data=raw)+
+    
+    aes(x=Total)+
+    
+    geom_histogram(bins=80)+
+    
+    scale_x_continuous(limits = c(0,50),expand=c(0,0))
+   
+  histo
+}
+
+nrow(raw %>% filter(Total>=50))
