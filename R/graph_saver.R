8fe299f2eaa339d4a893e584c34f7dcd4e19e3e9
Aeranthos <92371087+Nathan-303@users.noreply.github.com>
2022-10-25 13:25:06 +0100

    graph saver prototype

--- R/graph_saver.R
+++ R/graph_saver.R
@@ -1,41 +1,59 @@
-grapher <- function(plot, filename,file_format,scaling,type){
+#' Save ggplot2 graphs with sensible scaling and size options ready for publication
+#'
+#' @param plot The ggplot2 object to plot
+#' @param filename The location to save the file to
+#' @param file_format The format to save using, this can include agg_png and the
+#'  agg family
+#' @param type The graph size to save to 1 is a single column figure on a two 
+#' column page, 2 is a full page spread, both use a 16:9 aspect ratio, 3 is an 
+#' entire page
+#` @keywords faceted, sources
+#' @export
+#' @examples
+#' grapher()
+
+grapher <- function(plot, filename,file_format,type){
  
   
   
   if(grepl("agg_",file_format)==TRUE){
     file_format1 <- gsub(pattern="agg_",x=file_format,replacement = ""),
-    file_format <- (eval(parse(text=file_format)))}else{
-# 1 column graphs
-      ggsave(filename= paste0("scalehunt.png"),
-             plot=hmm,
+    file_format <- (eval(parse(text=file_format)))}
+
+  if (type==1){
+  # 1 column graphs
+      ggsave(filename= filename,
+             plot=plot,
              width=8.3,
              height=4.7,
              units = "cm",
              dpi = 600,
-             device=agg_png,
-             scaling=0.2)
+             device=file_format,
+             scaling=0.2)}
+  
+  if (type==2){
 # 2 column graphs
-      ggsave(filename= paste0("scalehuntbeeeg.png"),
-             plot=hmm,
+      ggsave(filename= filename,
+             plot=plot,
              width=17.1,
              height=9.6,
              units = "cm",
              dpi = 600,
-             device=agg_png,
+             device=file_format,
              scaling=0.3)
+  }
 # Large faceted graphs
-      
+   if (type==3){
+     ggsave(filename= filename,
+            plot=plot,
+            width=210,
+            height=297,
+            units = "mm",
+            dpi = 600,
+            device=file_format,
+            scaling=0.3) 
+   }   
 
       
     }
 }
-  #
-  
-  
-  filename= paste0(proc_tag,"/medmeancomp.",file_format1),
-plot=stat_facet,
-width=8.3,
-height=4.7,
-units = "cm",
-dpi = dpi,
-device=file_format
