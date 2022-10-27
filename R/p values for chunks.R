869fb8345d3ecd5957f66363a700956a7311d23d
Aeranthos <92371087+Nathan-303@users.noreply.github.com>
2022-10-27 13:40:08 +0100

    got p values

---
+++ R/p values for chunks.R
@@ -0,0 +1,42 @@
+library(WGCNA)
+
+data <- read.csv(prawn_path,
+                 row.names=1,
+                 check.names=FALSE)
+
+
+
+test <- sample(rep.int(x=c(1:384),times=73),size=nrow(data),replace=FALSE)
+
+chunkable <- bind_cols(data,test) %>% rename(chunk=`...85`) %>% group_by(chunk)
+
+long_data <- chunkable %>% pivot_longer(
+  cols=c("Agricultural","Domestic combustion","Energy production",
+         "Industrial combustion","Industrial production","Natural",
+         "Other transport and mobile machinery","Road transport","Solvents","Total"
+         ,"Waste treatment and disposal","Point sources"),
+  names_to = "Emission_source",
+  values_to = "emissions") %>%
+  group_by(Emission_source,chunk) %>%
+  mutate(emissions=replace_na(emissions,0))
+
+
+hmm <- long_data %>% do(tidy(lm(emissions~IMD,data=.)))
+
+aha <- hmm %>% pivot_wider(names_from=term,
+            values_from=c(estimate,
+                          p.value,
+                          std.error,
+                          statistic))
+
+reveal <- ggplot(data=aha)+
+  aes(x=p.value_IMD)+
+  geom_histogram(boundary=0,bins = 60)+
+  facet_wrap(~Emission_source,scale="free_y")+
+  coord_cartesian(xlim=c(0,1),expand=FALSE)+geom_vline(xintercept=0.05,colour="orange")
+
+
+
+q <- p.a384
+
+
