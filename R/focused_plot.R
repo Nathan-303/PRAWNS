7a0368ae8b1e896f0347835010c2bb7601aa83e8
Aeranthos <92371087+Nathan-303@users.noreply.github.com>
2022-11-07 11:44:39 +0000

    Began adding new lines

--- R/focused_plot.R
+++ R/focused_plot.R
@@ -1,18 +1,22 @@
 
-focused_plot()
+focused_plot <- function(focused_prawn_path){
 
-boxxy <- londonless_prawn %>% group_by(IMD) %>% summarise(q90=quantile(Total,c(0.90)),
+data <- active_stack <- read.csv(focused_prawn_path,
+                                 check.names = FALSE,
+                                 row.names = 1)
+
+boxxy <- active_stack %>% group_by(IMD) %>% summarise(q90=quantile(Total,c(0.90)),
                                                           q10=quantile(Total,c(0.10)),
                                                           q1=quantile(Total,c(0.25)),
                                                           q3=quantile(Total,c(0.75)),
                                                           med=quantile(Total,c(0.5))) %>%
   pivot_longer(cols=c(q90,q10,q1,q3,med),values_to = "Emissions")
 
-londonless_long_prawn <- londonless_prawn %>% pivot_longer(cols=c("Total","Road transport","Domestic combustion"),
+focused_long_prawn <- active_stack %>% pivot_longer(cols=c("Total","Road transport","Domestic combustion"),
                                                            values_to = "Emissions",
                                                            names_to="Source")
 
-london_window <- ggplot(data = londonless_long_prawn)+
+focused_window <- ggplot(data = focused_long_prawn)+
   aes(x=IMD,
       y=Emissions)+
   geom_boxplot(data=boxxy,aes(x=IMD,group=IMD,y=Emissions)) +
@@ -22,8 +26,22 @@ london_window <- ggplot(data = londonless_long_prawn)+
                 formula=y~x,
                 aes(colour=Source))+
 
-  geom_smooth(method="lm",
+  geom_smooth(data=focused_long_prawn %>% filter(Source=="Total"),
+              method="lm",
               formula=y~x,
               se=FALSE,
               show.legend=FALSE,
-              aes(linetype="Linear regression"))+
+              aes(x=IMD,
+                  y=Emissions,
+                  linetype="dashed"),
+              colour="blue")+
+
+  geom_line(data=focused_long_prawn %>% filter(Source=="Total"),
+            stat="summary",
+            aes(x=IMD,
+                y=Emissions,
+                linetype="dotted"),
+            colour = "blue")
+
+focused_window
+}
