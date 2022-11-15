1dcdd41e7fab57ce5841bc427647f3aa73a1c5d2
Aeranthos <92371087+Nathan-303@users.noreply.github.com>
2022-11-15 14:15:40 +0000

    Cleared out redundant packages

--- R/Startup.R
+++ R/Startup.R
@@ -1,7 +1,6 @@
 #' @export
 #Load in all ther packages as a jerry rigged fix to figuring out namespace
 Startup <- function(){
-library(raster)
 library(sf)
 library(tidyverse)
 library(terra)
@@ -9,9 +8,7 @@ library(exactextractr)
 library(rlang)
 library(broom)
 library(ggpubr)
-library(colorspace)
 library(viridis)
-library(googledrive)
 library(stars)
 library(devtools)
 library(roxygen2)
