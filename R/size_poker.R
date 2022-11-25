55ee4e19f508771e5e59911a1b564e439fb6ebfe
Aeranthos <92371087+Nathan-303@users.noreply.github.com>
2022-11-25 15:58:55 +0000

    sufficiently advanced graph

--- R/size_poker.R
+++ R/size_poker.R
@@ -15,12 +15,12 @@ size_poker <- function(prawn_path,pollutant,year){
 
   reformed_data <- data %>%
     #aggregate all non
-    mutate(Other_sources=Solvents+Natural+`Energy production`+`Waste treatment and disposal`+`Agricultural`+
+    mutate(`Other sources`=Solvents+Natural+`Energy production`+`Waste treatment and disposal`+`Agricultural`+
              `Point sources`+`Domestic combustion`+`Industrial combustion`+`Industrial production`+
            `Other transport and mobile machinery`) %>%
 
     pivot_longer(
-      cols=c("Road transport","Total","Other_sources"),
+      cols=c("Road transport","Total","Other sources"),
       names_to = "Emission_source",
       values_to = "emissions") %>%
     #sort into thirds based on IMD rank
@@ -49,7 +49,11 @@ almalgm <- reformed_data %>% summarise(emissions=mean(emissions))
                        expand=expansion(0,0))+
 
     labs(x=expression(atop("LSOA expanse decile","Area of the largest LSOA in this decile /"~km^2)),
-              y=bquote("Mean "~.(pollutant)~"emissions in "~.(year)~"/ tonnes "~km^2))
+              y=bquote("Mean "~.(pollutant)~"emissions in "~.(year)~"/ tonnes "~km^2))+
+
+    scale_colour_manual(breaks=c("Total","Road transport","Other sources"),
+                        values=c("black","blue","orange"),
+                        name="Emission source")
 
 
 
