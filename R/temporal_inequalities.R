bfebda1ca07b48f4c86136ae2c059799489a3df9
Aeranthos <92371087+Nathan-303@users.noreply.github.com>
2023-02-07 15:10:26 +0000

    initiated temporal inequalities

---
+++ R/temporal_inequalities.R
@@ -0,0 +1,16 @@
+#' Plot the progression in pollutant inequality over time
+#'
+#' @param time_range the years to be used, used for tracking filenames
+#' @param pollutant the pollutant being investigated, this should be in quotes and correspond with its name in the data
+#' @param name_generator the procedure to generate the input filename, should be a function with year as the imput
+#` @keywords faceted, sources
+#' @export
+#' @examples
+#' temporal_inequalities()
+#' 
+
+temporal_inequalities <- function(time_range,pollutant,name_generator){
+  
+  
+  
+}
