db4f57c5369cb672e7285038db23cec337f70c12
Nathan Gray <nrg508@york.ac.uk>
2023-03-20 15:41:09 +0000

    Got it working with a boxplot

--- R/facet_ethnicity_quintile_src.R
+++ R/facet_ethnicity_quintile_src.R
@@ -33,63 +33,103 @@ foray <- edata %>%
   mutate(Diversity_quintile = ntile(x=homogeneity,
                                     n=5))
 
+#Create a column for naming the facets
+Diversity_bounds <- quantile(foray$homogeneity,probs=seq(0,1,0.2)) %>% signif(2)*100
+Diversity_brackets <- vector()
+for(index in c(1:5)){
+  Diversity_brackets[index] <- paste0(Diversity_bounds[index],
+                                  "% to ",Diversity_bounds[index+1],"%")  
+}
+
+Diversity_tibble <- tibble(Diversity_quintile=c(1,2,3,4,5),caption=Diversity_brackets)
 plottable <- inner_join(
   x=data,
   y=foray,
   by=c("LSOA11CD"="geography code")
-)
-
+) %>% 
+#Bind on the captions
+  inner_join(
+    y=Diversity_tibble,
+    by="Diversity_quintile"
+  )
+#Create stats for boxplots
+boxxy <- plottable %>% group_by(IMD,caption) %>% 
+  summarise(q90=quantile(Total,c(0.90)),
+            q10=quantile(Total,c(0.10)),
+            q1=quantile(Total,c(0.25)),
+            q3=quantile(Total,c(0.75)),
+            med=quantile(Total,c(0.5))) %>%
+  pivot_longer(cols=c(q90,q10,q1,q3,med),values_to = "emissions")
 #Plot a faceted graph
 
-City_profile <- ggplot(data=plottable)+
+output <- ggplot(data=plottable
+)+
+  
   aes(x=IMD,
       y=Total)+
   
-  scale_x_continuous(
-    breaks=c(1:10),
-    minor_breaks = FALSE,
-    expand = expansion(mult=0,add=0))+
+  facet_wrap(~caption
+  )+
   
-  #Plot the line of best fit for the mean
+  geom_boxplot(data=boxxy,
+               inherit.aes=FALSE,
+               aes(x=IMD,
+                   y=emissions,
+                   group=IMD),
+               coef=10000000000000000000000000000000000000000000000000000000000000)+
+  
+  geom_line(stat="summary",
+            aes(linetype="Average points",colour="Mean"),
+            
+            fun=mean,
+            na.rm=TRUE
+  )+
+  
+  
   geom_smooth(method="lm",
               formula=y~x,
               se=FALSE,
-              show.legend=FALSE,
-              aes(color='Mean'))+
-  
-  #Plot a line passing through the mean at each decile
-  geom_line(stat="summary",
-            aes(color='Mean'),
-            fun=mean)+
+              aes(linetype="Linear regression",colour="Mean"),
+              
+              size=1,
+              na.rm = TRUE)+
   
   #Plot the line of best fit for the median
   geom_quantile(quantiles=0.5,
-                aes(color='Median'),
-                size =1,
-                formula=y~x,)+
+                aes(linetype="Linear regression",colour="Median"),
+                
+                formula=y~x,
+                size=1,
+                na.rm=TRUE)+
+  
+  
   
   #Plot a line through the medians for each decile
   geom_line(stat="summary",
             fun=median,
-            aes(color='Median'))+
+            aes(linetype="Average points",colour="Median"),
+            
+            na.rm=TRUE)+
   
-  #Plot a regression line for the whole UK for comparison
-  geom_smooth(data=read.csv(prawn_path) %>% tibble %>% dplyr::select(-c(TCITY15NM,Area))
-              , aes(
-                x=IMD,
-                y=Total,
-                color='UK mean'),
-              method="lm",
-              formula=y~x,
-              se=FALSE)+
+  scale_x_continuous(
+    breaks=c(1:10),
+    expand = expansion(mult=0,add=0),
+    minor_breaks = FALSE)+
   
-  scale_colour_manual(name="Average used",
-                      values = c("Median"="royalblue","Mean"="#FB8022FF",'UK mean'='black'))+
+  scale_colour_manual(values = c("Median"="royalblue","Mean"="#FB8022FF"),
+                      name= "Average used")+
   
-  labs(x="IMD decile where 10 is least deprived",
-       y=bquote("Average "~.(pollutant)~"emissions/ tonnes "~km^"-2"))+
   
-  theme(legend.position = "bottom")+
+  scale_linetype_manual(name= "Line plotted:",
+                        values = c("Linear regression"="solid","Average points"="dashed"),
+                        guide=guide_legend(override.aes = list(linetype=c("solid","dashed"),colour="black",shape=c(NA,NA),size=c(1,1)))
+  )+
   
-  facet_wrap(~Diversity_quintile)
+  labs(x=paste0("IMD decile where 10 is least deprived"),
+       y=bquote("Average "~.(pollutant)~"emissions in "~.(year)~"/ tonnes "~km^"-2"),
+       title=NULL
+  )+
+  theme(legend.position = "bottom",legend.key.width = unit(1.5,"cm"))
+
+output
 
