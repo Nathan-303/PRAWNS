431ed43c0f8e9dcb3fa7305e9690e3644bf4f8fd
Nathan Gray <nrg508@york.ac.uk>
2023-06-06 13:22:32 +0100

    Update process_tempura_prawns.R

--- R/process_tempura_prawns.R
+++ R/process_tempura_prawns.R
@@ -66,12 +66,15 @@ print("vect call")
   vectorised_shapefile <- vect(LSOA_shapefile)
 
   output <- tibble(LSOA11CD=vectorised_shapefile$LSOA11CD)
+
 print("still alive")
+
 index <- c(1:length(vectorised_shapefile))
+
 transient <- sf::st_as_sf(vectorised_shapefile[index])
   for(source_number in 1:length(filelist)){
     incProgress(1/length(filelist), detail = paste("Processing rasters this will take a while, currently on source ", source_number, " of ",length(filelist) ))
-    unzip only the layer being extracted to minimise memory use
+    #unzip only the layer being extracted to minimise memory use
     transient_raster <- unzip(zipfile = raster_path,
                               files=filelist[source_number]) %>%
       rast()%>%
