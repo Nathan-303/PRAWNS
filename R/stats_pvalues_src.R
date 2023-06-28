94ca1bed561623465edce9d34644e1e4059c6458
Nathan Gray <nrg508@york.ac.uk>
2023-06-29 00:00:58 +0100

    can produce an output

--- R/stats_pvalues_src.R
+++ R/stats_pvalues_src.R
@@ -14,7 +14,8 @@
 stats_pvalues_src <- function(prawn_path){
   data <- read.csv(prawn_path,
                  row.names=1,
-                 check.names=FALSE)
+                 check.names=FALSE) %>%
+    dplyr::select(-c(Tot_area,Offshore))
 
 
 if (nrow(data)<=73*384){
@@ -44,10 +45,30 @@ aha <- hmm %>% pivot_wider(names_from=term,
                           statistic))
 
 #Perform a benjamnini hochberg adjustment on the p values
-ben_hoch <- aha %>% mutate(adjusted=p.adjust(p.value_IMD,method="fdr"))
+ben_hoch <- aha %>% group_by(Emission_source) %>%
+  mutate(adjusted=p.adjust(p.value_IMD,method="fdr"))
 
-reveal <- ggplot(data=ben_hoch)+
-  aes(x=adjusted)+
+plist <- aha %>% ungroup() %>%
+ # filter(Emission_source=="Total") %>%
+  dplyr::select(p.value_IMD,Emission_source) %>%
+  group_by(Emission_source)
+
+ultimatelist <- plist %>% group_map(~p.adjust(.x$p.value_IMD,
+                                        method = "fdr"))
+
+
+ultimatetibble <-  tibble(placeholder <- c(1:length(ultimatelist[[1]])))
+
+for(source in c(1:nrow(group_keys(plist)))){
+  name <- paste0(group_keys(plist)[source,1])
+  ultimatetibble[[name]] <- ultimatelist[[source]]
+}
+
+plottable <- ultimatetibble %>%
+  pivot_longer(cols = c(2:ncol(ultimatetibble)),
+               names_to = "Emission_source")
+reveal <- ggplot(data=plottable)+
+  aes(x=value)+
   geom_histogram(boundary=0,bins = 60)+
   facet_wrap(~Emission_source,scale="free_y")+
   coord_cartesian(xlim=c(0,1),expand=FALSE)+geom_vline(xintercept=0.05,colour="orange")+
@@ -56,6 +77,29 @@ reveal <- ggplot(data=ben_hoch)+
 }else{
   reveal <- ggplot(data)+geom_bar(aes(x=IMD))+labs(title="Not here")
 }
+alpha <- 0.05
+pvector <- aha %>% group_by(Emission_source) %>% dplyr::select(Emission_source,p.value_IMD)
+#arrange them in order
+sorted_pvector <- pvector %>%
+  filter(Emission_source=="Total") %>%
+  arrange(by.group=TRUE,p.value_IMD)
+
+m <- nrow(sorted_pvector)
+test <- sorted_pvector %>%
+  mutate(k=c(1:m)) %>%
+  filter(p.value_IMD<=((k/m)*alpha))
+
+
+#plot the results
+  ggplot(sorted_pvector)+
+
+    aes(x=k,y=p.value_IMD)+
+
+    geom_point()+
+
+    geom_abline(slope = )
+
+#Find the first one that fulfils the requirement
 
 
 reveal
