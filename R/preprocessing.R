650485694c010d128243171435e38dbf225bb1f7
Nathan Gray <nrg508@york.ac.uk>
2024-03-06 12:36:35 +0000

    general progress

--- R/preprocessing.R
+++ R/preprocessing.R
@@ -1,7 +1,56 @@
+library(PRAWNSdata)
+pollutant <- "NOx"
 #loop for the years
 for(year in c(2010:2020)){
+
+  filename <- paste0(year,
+                     "_",
+                     pollutant,
+                     "_PRAWN.csv")
   #
-  filename <- 
-  #check if the file is downloaded already
-  if(file.exists("whatever its called"))
+  selected_key <- pollutant_key %>% filter(Pollutant==pollutant)
+  #check if the final file has already been made
+  if(file.exists(filename)==FALSE){
+    #download the file
+    #Download the pollutant emission maps
+    file_url=paste0(
+      "https://naei.beis.gov.uk/mapping/mapping_",
+      year,
+      "/",
+      selected_key$ID.number,
+      ".zip")
+
+    ziploc <- paste0(
+      "Data/Autodownloads/",
+      year,
+      "_",
+      pollutant,".zip")
+    #Download the zip to a temporary location
+    download.file(url=file_url,
+                  destfile=here::here(ziploc))
+                  )
+
+
+
+    selected_key <- pollutant_key %>% filter(Pollutant==input$Pollutant)
+
+    incProgress(amount=0.1,detail="Downloading emissions maps")
+
+    input_prawn <- process_tempura_prawns(
+      raster_path = here::here(ziploc),
+      pollutant_data_name = selected_key$ID.string,
+      year = as.numeric(input$year),
+      pollutant = input$Pollutant,
+      output_path = here::here(paste0(input$Pollutant," emissions in ",input$year,"PRAWN.csv"))
+    )
+
+    #create prawns
+    process_create_set_prawns(
+      year=year,
+      raster_path = ,
+
+
+    )
+
+  }
 }
--- R/process_create_set_prawns.R
+++ R/process_create_set_prawns.R
@@ -35,7 +35,6 @@
 #'   pollutant_data_name="nox",
 #'   year="2019",
 #'   pollutant="NOx")
-shapefile_path <- "Data/Historic_stats/2011_LSOA"
 
 process_create_set_prawns <- function(raster_path="undefined",
                           tif_path="undefined",
@@ -44,8 +43,7 @@ process_create_set_prawns <- function(raster_path="undefined",
                           output_path="undefined",
                           pollutant_data_name,
                           year,
-                          pollutant,
-                          is_raw=FALSE){
+                          pollutant){
 
 # Calculate the average pollution for each area ----------------------------
   #names that the LSOA column can have
@@ -53,150 +51,47 @@ process_create_set_prawns <- function(raster_path="undefined",
 
 #Three chained if statements, which trigger if there is a path to the appropriate file in the function call
   #If its'a raster
-  if(raster_path!="undefined"){
   #Create a list of all the raster files present in the folder specified by raster_path
-  filelist <- list.files(raster_path,
-    pattern = ".asc",
-    full.names = TRUE)
+  filelist <- grep('\\.asc$', unzip((raster_path), list=TRUE)$Name,
+                   ignore.case=TRUE, value=TRUE)
 
-  #Store the rasters as a stack
-  source_stack <- rast(filelist[1]) %>% terra::subst(NA,0)
-  for(index in 2:length(filelist)){
-    source_stack <- c(source_stack,rast(filelist[index])%>% terra::subst(NA,0))
-  }
-  }
-  #If its a tif
-  if(tif_path!="undefined"){
-    source_stack <- read_stars("Data/2019_modelled_NOx.tif") %>% rast()
 
-  }
-  #If it's a csv
-  if(csv_coordinates_path!="undefined"){
-    print(csv_coordinates_path)
-    csv_raster <- read.csv(file=csv_coordinates_path,
-                           skip = 5,
-                           row.names=1,
-                           check.names=FALSE) %>% tibble()
-  print("csv made")
-    base_raster <- rasterFromXYZ(csv_raster)
-  print("raster next")
-    source_stack <- rast(base_raster)
-  }
 print("vect call")
-  #Read the shapefile
-  LSOA_shapefile <- vect(shapefile_path)
-  LSOA_field <- intersect(potentials,as.character(names(LSOA_shapefile)))
-    #potentials%in%names(LSOA_shapefile)
-  LSOA_shapefile <- LSOA_shapefile %>% tidyterra::rename("LSOA"=LSOA_field)
-print("index call")
-  #Calculate the average for each polygon in the shapefile
-  index <-  c(1:length(LSOA_shapefile))
-  print("stftft call")
-  transient <- sf::st_as_sf(LSOA_shapefile[index])
-  print("exactextract call")
-  pollution_mean <- exact_extract(source_stack,transient,'mean')
-  #Output the results as a tibble containing the indexed position, the pollution mean and the LSOA code, a property from the shapefile that enables binding on LSOA statistics
-  print("output call")
-  output <- tibble(poll_mean=pollution_mean,
-                   LSOA=LSOA_shapefile[LSOA],
-                   expanse=expanse(LSOA_shapefile)
-                   ) %>% unnest(poll_mean)
+#Read the shapefile
+LSOA_shapefile <- vect(shapefile_path)
+LSOA_field <- intersect(potentials,as.character(names(LSOA_shapefile)))
+#potentials%in%names(LSOA_shapefile)
+LSOA_shapefile <- LSOA_shapefile %>% tidyterra::rename("LSOA"=LSOA_field)
+
+output <- tibble(LSOA=vectorised_shapefile$LSOA)
+
+print("still alive")
+
+index <- c(1:length(vectorised_shapefile))
+
+transient <- sf::st_as_sf(vectorised_shapefile[index])
+for(source_number in 1:length(filelist)){
+
+  #unzip only the layer being extracted to minimise memory use, skip offshore
+  transient_raster <- unzip(zipfile = raster_path,
+                            files=filelist[source_number],
+                            exdir=here::here(tempdir())) %>%
+    rast()%>%
+    terra::subst(NA,0)
 
 
-  if(csv_coordinates_path!="undefined"){
-    return(output)
-  }
-  #output a csv with minimum processing
-  if(output_path!="undefined"){
-    print("outputting?")
-    if (is_raw==TRUE){
-      print("writing file")
-    write.csv(file=output_path,
-              x = output)
+  pollution_mean <- exact_extract(transient_raster,transient,'mean') %>%
+    tibble() %>% signif(digits=4)
 
-      return(output)
-    }else{
+  unlist(here::here("tempdir"))
+  #rename the column for smoother binding, dplyr rename not used because it was being awkwa
+  colnames(pollution_mean)[1]=filelist[source_number]
 
-        write.csv(file=output_path,
-                    x = output)}}
+  output <- pollution_mean %>% bind_cols(output)
+}
 
 
-# Read the additional data as a list of tibbles----------------------------------------------------------------
-
-  rural_urban <- read.csv("Data/LSOA_statistics/LSOA_urban_rural.csv") %>%
-    tibble %>%  dplyr::select(-FID)
-
-  city_data <- read.csv("Data/LSOA_statistics/city lookup 2011.csv") %>%
-    tibble()
-  #Renames a column to avoid a special character that makes things go wrong
-  colnames(city_data)[1] <- "LSOA21CD"
-
-  #Reads the demographic information about the LSOAs, binds them by LSOA code so the FID is incorporated
-  LSOA_demographics <- read.csv("Data/LSOA_statistics/2019_LSOA_Stats.csv") %>%
-    tibble() %>%
-    rename(LSOA11CD=LSOA.code..2011.) %>%
-    inner_join(city_data,by="LSOA11CD")%>%
-    inner_join(rural_urban,by=c("LSOA11CD"="LSOA11CD"))
-
-  #Links the demographic data to the references to the shapefile
-  demo_linked_reference <- inner_join(city_data,LSOA_demographics,by=c("LSOA11CD"="LSOA11CD","FID"="FID","TCITY15CD"="TCITY15CD",
-                                                                       "TCITY15NM"="TCITY15NM"))
-
-  #Reads the county lookup data
-  county_lookup <- read.csv("Data/LSOA_statistics/county lookup 2019.csv",row.names = 1)
-
-  #Makes a chunk for the LSOAs that are in counties, then mutates in a column saying it's a couty not UA
-  county_chunk <- inner_join(county_lookup,demo_linked_reference,by=c("LAD19NM"="Local.Authority.District.name..2019.")) %>%
-    mutate(Area_Type="County") %>%
-    rename("Area"=CTY19NM)
-
-  unitary_list <- !(LSOA_demographics$Local.Authority.District.name..2019. %in% county_lookup$LAD19NM)
-
-  unitary_chunk <- LSOA_demographics[unitary_list,] %>%
-    mutate(Area_Type="Unitary Authority",Area=Local.Authority.District.name..2019.)# %>%
-  #inner_join(demo_linked_reference,by="LSOA11CD")
-
-  rm(unitary_list)
-  refined_chunk <- bind_rows(unitary_chunk,county_chunk) %>% tibble()
-  rm(unitary_chunk)
-  rm(county_chunk)
-
-#add a conversion column for 2011 LSOA codes
-  keys <- read.csv("Data/LSOA_statistics/LSOA_porter.csv",
-                   check.names=FALSE)
-
-  conversion <- inner_join(refined_chunk,keys,by="LSOA11CD")
-
-  tidy <- conversion %>% group_by(LSOA21CD) %>%
-    summarise(IMDrank=mean(Index.of.Multiple.Deprivation..IMD..Rank..where.1.is.most.deprived.),
-                           RUC11=RUC11,
-              LSOA11CD=LSOA11CD) %>% ungroup() %>%
-    mutate(IMD=ntile(x=IMDrank,
-                     n=10))
-
-  removebroken <- tibble(broken=c(rep(0,times=20),1,0,0,1))
-
-  takeout <- tidy %>%group_by(LSOA21CD) %>%
-    filter(n()>1) %>%
-    group_by(LSOA21CD,RUC11) %>%
-    filter(n()==1) %>%
-    mutate(RUC11="mismatch") %>%
-    cbind(removebroken)
-           #remove the weird dupe column where it assigns part of gateshead to northumberland, and vice
-           #versa in addition to where they should actually be. This needs to be done by inserting a killswitch
-
-
-  superrefined <- dplyr::rows_update(#usew the killswitch to remove the broken bits
-    tidy %>% mutate(broken=0) %>% anti_join(takeout %>% filter(broken==1),by=c("LSOA21CD","LSOA11CD"))
-
-
-                                     ,takeout %>% filter(broken==0), by="LSOA11CD")
-# Combine the pollution means with the additional data --------------------
-
-
-  prawns <- inner_join(output,superrefined,by="LSOA21CD")
-
-  if(raster_path!="undefined"){
+prawns <- output
 
     renamer <- function(data,last_two_digits_year,pollutant_data_name){
 
@@ -205,7 +100,9 @@ print("index call")
                      "Offshore","Other transport and mobile machinery","Road transport","Solvents","Tot_area","Total",
                      "Waste treatment and disposal")
 
-      Nmdlst <- paste0("mean.",c("agric","domcom","energyprod","indcom","indproc","nature","offshore","othertrans","roadtrans","solvents","totarea","total","waste"),pollutant_data_name,last_two_digits_year)
+      Nmdlst <- paste0(c("agric","domcom","energyprod","indcom","indproc","nature","offshore","othertrans","roadtrans","solvents","totarea","total","waste"),
+                       pollutant_data_name,last_two_digits_year,
+                       ".asc")
 
       core <- tibble(Output=NamedList,Match=Nmdlst)
 
@@ -236,8 +133,20 @@ print("index call")
   )
 print("Does it get here")
   prawns <- prawns %>% mutate("Point sources"=Total-Tot_area)
-  }
 
+  # Read the additional data ----------------------------------------------------------------
+  set_allocator <- read.csv("Data/Historic_stats/YearIMDCensus.csv")
+
+  chosen_set <- set_allocator %>% dplyr::filter(Year==year) %>% select(Set) %>% as.character()
+
+  #read in the set data, this is where any extra data should be added
+  setdata <- read.csv(paste0("Data/Historic_stats/set",
+                             chosen_set,
+                             ".csv"))
+  # Combine the pollution means with the additional data --------------------
+
+
+  prawns <- inner_join(output,setdata,by="LSOA")
 
 # Output the results ------------------------------------------------------
 print("Down to the save")
