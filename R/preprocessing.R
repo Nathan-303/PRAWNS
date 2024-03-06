d3399d00619974c48d0f5ca9429d3cf506cc00e1
Nathan Gray <nrg508@york.ac.uk>
2024-03-06 13:41:05 +0000

    changed destinations and tidied

--- R/preprocessing.R
+++ R/preprocessing.R
@@ -1,17 +1,31 @@
 library(PRAWNSdata)
 pollutant <- "NOx"
+
+
+#read the table used for assigning years and shapefiles
+setlinks <- read.csv(file="Data/Historic_stats/YearIMDCensus.csv")
 #loop for the years
 for(year in c(2010:2020)){
-
-  filename <- paste0(year,
+#select the correct protion of the lookup table
+  chosenset <- setlinks %>% dplyr::filter(Year==year)
+  filename <- paste0("Data/Historic_PRAWNS/",
+                     year,
                      "_",
                      pollutant,
                      "_PRAWN.csv")
   #
   selected_key <- pollutant_key %>% filter(Pollutant==pollutant)
+
+
   #check if the final file has already been made
   if(file.exists(filename)==FALSE){
-    #download the file
+    ziploc <- paste0(
+      "Data/Autodownloads/",
+      year,
+      "_",
+      pollutant,".zip")
+    #check if the emission maps are downloaded
+    if(file.exists(ziploc)==FALSE){
     #Download the pollutant emission maps
     file_url=paste0(
       "https://naei.beis.gov.uk/mapping/mapping_",
@@ -20,37 +34,28 @@ for(year in c(2010:2020)){
       selected_key$ID.number,
       ".zip")
 
-    ziploc <- paste0(
-      "Data/Autodownloads/",
-      year,
-      "_",
-      pollutant,".zip")
     #Download the zip to a temporary location
     download.file(url=file_url,
-                  destfile=here::here(ziploc))
-                  )
+                  destfile=(ziploc))
+    }
 
 
-
-    selected_key <- pollutant_key %>% filter(Pollutant==input$Pollutant)
-
-    incProgress(amount=0.1,detail="Downloading emissions maps")
-
-    input_prawn <- process_tempura_prawns(
-      raster_path = here::here(ziploc),
+    input_prawn <- process_create_set_prawns(
+      raster_path = ziploc,
       pollutant_data_name = selected_key$ID.string,
-      year = as.numeric(input$year),
-      pollutant = input$Pollutant,
-      output_path = here::here(paste0(input$Pollutant," emissions in ",input$year,"PRAWN.csv"))
+      year = year,
+      pollutant = pollutant,
+      output_path = paste0(pollutant," emissions in ",year,"PRAWN.csv"),
+      shapefile_path = paste0("Data/Historic_stats/",
+                              as.character(chosenset$LSOAs),
+                              "_LSOA")
     )
 
-    #create prawns
-    process_create_set_prawns(
-      year=year,
-      raster_path = ,
+  write.csv(x=input_prawn,
+            file=filename)
 
 
-    )
 
   }
 }
+
